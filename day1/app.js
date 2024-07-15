854e0a98f1f8cf9b940160f275fd447064a7e49c
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-15 12:30:27 +0500

    completed Q2

--- day1/app.js
+++ day1/app.js
@@ -1,9 +1,20 @@
 // Question 1
 
-let name = prompt("what is your name?");
-let age = prompt("what is your age?");
+// let name = prompt("what is your name?");
+// let age = prompt("what is your age?");
 
-alert(`${name} is ${age} years old`);
+// alert(`${name} is ${age} years old`);
 
 // Question 2
 let goldenString = prompt("Enter your string");
+
+if (
+  goldenString[0] == "A" ||
+  (goldenString[0] == "a" && goldenString.length == 5)
+) {
+  console.log(
+    "Your string identifies as a golden string and you have to accept it."
+  );
+} else {
+  console.log("Sorry, your string is straight.");
+}

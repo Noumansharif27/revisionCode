09b5c39dec7336bf70a7cac7af0b2b7a66e0c084
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-15 12:35:55 +0500

    colsing

--- day1/app.js
+++ day1/app.js
@@ -6,15 +6,26 @@
 // alert(`${name} is ${age} years old`);
 
 // Question 2
-let goldenString = prompt("Enter your string");
+// let goldenString = prompt("Enter your string");
 
-if (
-  goldenString[0] == "A" ||
-  (goldenString[0] == "a" && goldenString.length == 5)
-) {
-  console.log(
-    "Your string identifies as a golden string and you have to accept it."
-  );
+// if (
+//   goldenString[0] == "A" ||
+//   (goldenString[0] == "a" && goldenString.length == 5)
+// ) {
+//   console.log(
+//     "Your string identifies as a golden string and you have to accept it."
+//   );
+// } else {
+//   console.log("Sorry, your string is straight.");
+// }
+
+// Quesion 3
+// Check wether the two numbers have same last digit or not
+let numberOne = prompt("enter first number.");
+let numberSecond = prompt("enter second number.");
+
+if (numberOne.length - 1 == numberSecond.length - 1) {
+  console.log("Both of them have same last number.");
 } else {
-  console.log("Sorry, your string is straight.");
+  console.log("Both of them have different last numbrt");
 }

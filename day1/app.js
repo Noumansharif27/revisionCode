b29f1d68c6d30dcccb339692413ebc780b457480
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-15 12:25:40 +0500

    solving questions

---
+++ day1/app.js
@@ -0,0 +1,9 @@
+// Question 1
+
+let name = prompt("what is your name?");
+let age = prompt("what is your age?");
+
+alert(`${name} is ${age} years old`);
+
+// Question 2
+let goldenString = prompt("Enter your string");
